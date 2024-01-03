258a4470b8681a0a55dcb92913dddf3f36b60115
jask09 <jask09@yahoo.com>
2024-01-03 17:36:24 -0500

    urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
+  path('', views.signup, name='home'),
   path('accounts/signup/', views.signup, name="signup"),
+  path('jobs/', views.jobs_index, name='index'),
+  path('jobs/<int:job_id>/', views.jobs_detail, name='deatail'),
+  
+
 ]
