06d760d53a2ce732ad2468676eaebb795dff7200
jask09 <jask09@yahoo.com>
2024-01-03 14:55:24 -0500

    admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Connection, Job, Todo, Status, Interaction
 
 # Register your models here.
+admin.site.register(Connection)
+admin.site.register(Job)
+admin.site.register(Todo)
+admin.site.register(Status)
+admin.site.register(Interaction)
