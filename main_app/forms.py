a8b4ce29b7b8ea54339e7c6b863c807f1bfec7da
Emmett Verba <emmettverba@gmail.com>
2024-01-04 12:48:12 -0700

    creates backend CRUD capabilities for Status

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,12 @@
 from django.forms import ModelForm
-from .models import Todo
+from .models import Todo, Status
 
 class TodoForm(ModelForm):
     class Meta:
         model = Todo
         fields = ['description']
+
+class StatusForm(ModelForm):
+    class Meta:
+        model = Status
+        fields = ['description', 'date']
