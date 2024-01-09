03636a3347d07fca1e36f6d15aeee7adf8463b31
reina-duf <reina.dufrene@gmail.com>
2024-01-09 16:42:18 -0700

    adds functionality for associating jobs to a contact

--- main_app/views.py
+++ main_app/views.py
@@ -179,10 +179,12 @@ def connections_detail(request, connection_id):
   connection = Connection.objects.get(id = connection_id)
   interactions = Interaction.objects.filter(connection = connection_id)
   interaction_form = InteractionForm()
+  assoc_jobs = Job.objects.filter(connections__id__exact=connection_id)
   return render(request, 'connections/detail.html', {
     'connection' : connection,
     'interactions' : interactions,
-    'interaction_form' : interaction_form
+    'interaction_form' : interaction_form,
+    'assoc_jobs': assoc_jobs,
   })
 
 
